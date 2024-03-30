31b49a471a9978416b67b93e69989cf0f1def443
sofiane <sofiane@meriane.dev>
2024-03-30 18:05:27 +0100

    quizData updated

--- src/quizData.js
+++ src/quizData.js
@@ -5,6 +5,11 @@ const quizData = [
       answer: "Spacewar!"
     },
     {
+      question: "What was the first video game ever made?",
+      options: ["Pong", "Spacewar!", "Tetris", "Computer Space"],
+      answer: "Spacewar!"
+    },
+    {
       question: "Which company developed the first commercial antivirus software?",
       options: ["Symantec", "McAfee", "Norton", "Kaspersky Lab"],
       answer: "McAfee"
